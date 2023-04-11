8946890128a0f626beb1e920beae88bec02e7d05
José Francisco Dos Santos Neto <jf9718830@gmail.com>
2023-04-11 08:48:41 -0300

    Terminado o program

--- StreetFighter/Program.cs
+++ StreetFighter/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IStreetService, StreetService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
